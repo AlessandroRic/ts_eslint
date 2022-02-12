d859ace13ac5bf36099e42b5be69421a3eaa0880
Alessandro Ferreira <35277571+AlessandroRic@users.noreply.github.com>
2022-02-12 10:02:45 -0300

    adjust fuctions to receive database

--- src/modules/cars/useCases/listCategories/intex.ts
+++ src/modules/cars/useCases/listCategories/intex.ts
@@ -2,7 +2,7 @@ import { CategoriesRepository } from "../../repository/implementations/Categorie
 import { ListCategoriesController } from "./ListCategoriesController";
 import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
 
-const categoriesRepository = CategoriesRepository.getInstance();
+const categoriesRepository = null;
 const listCategoriesUseCase = new ListCategoriesUseCase(categoriesRepository);
 const listCategoriesControler = new ListCategoriesController(listCategoriesUseCase);
 
