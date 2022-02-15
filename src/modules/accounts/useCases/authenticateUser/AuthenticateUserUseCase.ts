b494b0cbaa04db63a77f6322b034194b96a87a62
Alessandro Ferreira <35277571+AlessandroRic@users.noreply.github.com>
2022-02-14 21:28:05 -0300

    add authentication

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,62 @@
+import { compare } from "bcrypt";
+import { inject, injectable } from "tsyringe";
+import { IUsersRepository } from "../../repository/IUsersRepository";
+import { sign } from "jsonwebtoken";
+
+interface IRequest {
+    email: string;
+    password: string;
+}
+
+interface IResponse {
+    user: {
+        name: string;
+        email: string;
+    },
+    token: string
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+
+    constructor(
+        @inject("UsersRepository")
+        private usersRepository: IUsersRepository
+    ){}
+
+    async execute({ email, password }: IRequest): Promise<IResponse> {
+
+        const user = await this.usersRepository.findByEmail(email);
+
+        if(!user) {
+            throw new Error("Email or password incorrect");
+        }
+
+        const passwordMatch = await compare(password.toString(), user.password);
+
+        if(!passwordMatch) {
+            throw new Error("Email or password incorrect");
+        }
+
+        const token = sign({
+
+        }, "caffg46sad853asd548as2",
+        {
+            subject: user.id,
+            expiresIn: "1d"
+        });
+
+        const tokenReturn: IResponse = {
+            token,
+            user: {
+                name: user.name,
+                email: user.email
+            }
+        }
+
+        return tokenReturn;
+        
+    }
+}
+
+export { AuthenticateUserUseCase }
