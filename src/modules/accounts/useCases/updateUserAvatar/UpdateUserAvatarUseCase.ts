1739eba6e031a01e528829d5928aacad3b1337bf
Alessandro Ferreira <35277571+AlessandroRic@users.noreply.github.com>
2022-02-20 20:28:29 -0300

    delete file on update

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from "tsyringe";
 import { IUsersRepository } from "../../repository/IUsersRepository";
+import { deleteFile } from "../../../../utils/file"
 
 interface IRequest {
     user_id: string;
@@ -16,6 +17,10 @@ class UpdateUserAvatarUseCase {
 
     async execute({ user_id, avatar_file }: IRequest): Promise<void> {
         const user = await this.userRepository.findById(user_id);
+
+        if (user.avatar) {
+            await deleteFile(`./tmp/avatar/${user.avatar}`);
+        }
     
         user.avatar = avatar_file;
 
